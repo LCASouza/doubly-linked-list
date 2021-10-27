133f9bf8669095f3d223ba931ad071c55e04ac3b
Lucas Souza <73047868+LCASouza@users.noreply.github.com>
2021-10-27 20:37:42 -0300

    Update list.h

--- list.h
+++ list.h
@@ -15,7 +15,6 @@ class List{
         bool pushFront(type *obj);
         bool pushBack(type *obj);
         bool popFront(type *obj);
-        bool popBack(type *obj);
         bool getFront(type *obj);
         bool getBack(type *obj);
         bool isEmpty();
